89ac407f054616ca160d9bbc8ef60e6234d0b16c
Alex <whitedawn1@hotmail.com>
2015-09-28 17:42:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/angel/bluegreenfragment/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.angel.bluegreenfragment;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int fragmentDisplayed = R.id.blue_fragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        updateFragment();
+
+        View v = findViewById(android.R.id.content);
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateFragment();
+            }
+
+        });
+    }
+
+    private void updateFragment(){
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        if (fragmentDisplayed == R.id.blue_fragment){
+            GreenFragment greenFragment = new GreenFragment();
+            ft.replace(android.R.id.content, greenFragment);
+            fragmentDisplayed = R.id.green_fragment;
+        }
+        else{
+            BlueFragment blueFragment = new BlueFragment();
+            ft.replace(android.R.id.content, blueFragment);
+            fragmentDisplayed = R.id.blue_fragment;
+        }
+        ft.commit();
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
